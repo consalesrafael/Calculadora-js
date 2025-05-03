6fed9e05e117de3cc98be0bf9fb715d3b0c0125c
consalesrafael <consalesrafael63@gmail.com>
2025-05-03 19:15:33 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function calcula(tipo, valor){
+    if(tipo === 'acao'){
+
+        if(valor==='c'){
+            document.getElementById("tela").value = ''
+        }
+        if(valor === '+' || valor === '/' || valor === '*' || valor === '-' || valor === '.'){
+            document.getElementById("tela").value += valor
+        }
+        if(valor === '='){
+            var resultado = eval(document.getElementById("tela").value)
+
+            document.getElementById("tela").value = resultado
+        }
+
+    }else if (tipo === 'valor'){
+        document.getElementById("tela").value += valor
+    }
+}
